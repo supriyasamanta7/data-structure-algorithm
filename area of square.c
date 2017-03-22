ee059e559588f9f6acafeed86e3c7a3fbc0b5054
supriya rani <supriyasamanta7@gmail.com>
2017-03-23 00:43:49 +0530

    Create area of square.c

---
+++ area of square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+   int side, area;
+ 
+   printf("\nEnter the Length of Side : ");
+   scanf("%d", &side);
+ 
+   area = side * side;
+   printf("\nArea of Square : %d", area);
+ 
+   return (0);
+}
